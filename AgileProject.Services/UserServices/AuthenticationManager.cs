237201cc474c859c899712a29cfed4f862d24d7c
SeanDumler <sean.dumler@outlook.com>
2023-06-21 20:29:00 -0500

    finishing user services

--- AgileProject.Services/UserServices/AuthenticationManager.cs
+++ AgileProject.Services/UserServices/AuthenticationManager.cs
@@ -1,4 +1,7 @@
+using AgileProject.Data.Entities;
+using AgileProject.Models.UserModels;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 
 namespace AgileProject.Services.UserServices
@@ -7,6 +10,25 @@ namespace AgileProject.Services.UserServices
     {
         private readonly IConfiguration _configuration;
         private readonly IMapper _mapper;
-        private readonly UserManager
+        private readonly UserManager<UserEntity> _userManager;
+        private string USERNAME = "";
+
+        public AuthenticationManager(IConfiguration configuration, IMapper mapper, UserManager<UserEntity> userManager)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        public Task<AuthResponse> Login(Login login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<IdentityError>> Register(UserEntityVM userEntity)
+        {
+            throw new NotImplementedException();
+        }
+        
     }
 }
