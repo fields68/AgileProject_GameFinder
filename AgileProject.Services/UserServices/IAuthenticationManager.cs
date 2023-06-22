04239d13b772df427acc24f157d28d5f09eb9edf
SeanDumler <sean.dumler@outlook.com>
2023-06-21 20:03:41 -0500

    minor user problems fixed

--- AgileProject.Services/UserServices/IAuthenticationManager.cs
+++ AgileProject.Services/UserServices/IAuthenticationManager.cs
@@ -5,6 +5,6 @@ namespace AgileProject.Services.UserServices
     public interface IAuthenticationManager
     {
         Task<IEnumerable<IdentityError>> Register(UserEntityVM userEntity);
-        Task<AuthResponse> Login(Login login)
+        Task<AuthResponse> Login(Login login);
     }
 }
