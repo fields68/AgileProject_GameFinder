99ea8d690f7e516ebcaf06e2abc0f0c4044c32e4
SeanDumler <sean.dumler@outlook.com>
2023-06-21 19:44:10 -0500

    User Entity Added

--- AgileProject.Data/Entities/UserEntity.cs
+++ AgileProject.Data/Entities/UserEntity.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 namespace AgileProject.Data.Entities
 {
     public class UserEntity : IdentityUser
     {
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        public string FirstName { get; set; } = string.Empty;
 
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+        public string LastName { get; set; } = string.Empty;
+        
+        public string FullName { get { return $"{FirstName} {LastName}"; } }
     }
 }
