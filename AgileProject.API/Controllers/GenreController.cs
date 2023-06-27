e9dcea571cef665694780177450b5dc30115f12f
Kevin Michael <kcamichael@gmail.com>
2023-06-27 18:02:46 -0400

    controller

--- AgileProject.API/Controllers/GenreController.cs
+++ AgileProject.API/Controllers/GenreController.cs
@@ -1,7 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AgileProject.API.Controllers
 {
-    public class GenreController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GenreController : ControllerBase
     {
-        // [httpGet]
+        
     }
 }
