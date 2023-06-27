8044069deff0a7f3d2942425f21410653721a59e
Clarissa Fields <32175821+fields68@users.noreply.github.com>
2023-06-26 21:17:28 -0400

    Update GameService.cs

--- AgileProject.Services/GameServices/GameService.cs
+++ AgileProject.Services/GameServices/GameService.cs
@@ -13,6 +13,16 @@ namespace AgileProject.Services.GameServices
             _context = context;
         }
 
+        public async Task<bool> DeleteGame(int id)
+        {
+            var game = await _context.Games.FirstOrDefaultAsync(g => g.Id == id);
+            if (game is null)
+                return false;
+            _context.Games.Remove(game);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<bool> UpdateGame(GameEdit model)
         {
             var game = await _context.Games.Include(game => game.Title).SingleOrDefaultAsync(g => g.Id == model.Id);
