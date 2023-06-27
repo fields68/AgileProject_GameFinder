c28240d3cc1e8a28beb757e38f319ce383bdbd72
Clarissa Fields <clarissa.alaze@gmail.com>
2023-06-26 22:01:46 -0400

    method for creating games added.

--- AgileProject.Services/GameServices/GameService.cs
+++ AgileProject.Services/GameServices/GameService.cs
@@ -1,5 +1,7 @@
 using AgileProject.Data.AgileProjectContext;
+using AgileProject.Data.Entities;
 using AgileProject.Models.GameModels;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 
 namespace AgileProject.Services.GameServices
@@ -7,10 +9,21 @@ namespace AgileProject.Services.GameServices
     public class GameService : IGameService
     {
         private readonly AgileProjectDBContext _context;
+        private readonly IMapper _mapper;
 
-        public GameService(AgileProjectDBContext context)
+        public GameService(AgileProjectDBContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> AddGame(GameCreate model)
+        {
+            var entity = _mapper.Map<GameEntity>(model);
+            {
+                await _context.Games.AddAsync(entity);
+                return await _context.SaveChangesAsync() > 0;
+            }
         }
 
         public async Task<bool> DeleteGame(int id)
